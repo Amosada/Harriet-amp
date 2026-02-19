4df1fad1f73d065191facc67c85e34298385a4fc
Amosada <106959592+Amosada@users.noreply.github.com>
2026-02-19 13:09:03 +0300

    Change page title from 'Good Morning' to 'Good day'

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Good Morning Beautiful..! 💝",
+    pageTitle: "Good day Beautiful..! 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
